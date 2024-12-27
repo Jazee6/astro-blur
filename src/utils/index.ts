3947eecc286f7f60786ab0e026af6d8a5eb153dd
Jazee6 <jazee@jaze.top>
2024-12-27 21:41:07 +0800

    feat: Astro 5

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,20 +1,4 @@
-import {getCollection} from "astro:content";
 import {siteConfig} from "../config.ts";
-
-export async function getIndexPosts() {
-    const posts = await getCollection('posts', ({data}) => {
-        return import.meta.env.PROD ? data.isDraft !== true : true
-    })
-    return posts.sort(
-        (a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf()
-    )
-}
-
-export async function getSlugPosts() {
-    return await getCollection('posts', ({data}) => {
-        return import.meta.env.PROD ? data.isDraft !== true : true
-    })
-}
 
 export function debounce(fn: Function, delay: number = 100) {
     let timeoutId: NodeJS.Timeout;
