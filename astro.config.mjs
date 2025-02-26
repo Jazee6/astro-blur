e0bcf732a2c825f8caf22206d66c7ec53e81a858
Jazee6 <jazee@jaze.top>
2025-02-26 12:56:20 +0800

    chore: update deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,18 @@
 import {defineConfig} from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import {remarkModifiedTime} from "./src/utils/remark-modified-time";
 import {siteConfig} from "./src/config";
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
     site: siteConfig.site,
-    integrations: [mdx(), sitemap(), tailwind({
-        applyBaseStyles: false
-    }), icon()],
+
+    integrations: [mdx(), sitemap(), icon()],
+
     markdown: {
         shikiConfig: {
             themes: {
@@ -21,7 +22,12 @@ export default defineConfig({
         },
         remarkPlugins: [remarkModifiedTime]
     },
+
     devToolbar: {
         enabled: false
+    },
+
+    vite: {
+        plugins: [tailwindcss()]
     }
 });
